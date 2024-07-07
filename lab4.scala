fd7ab7a7904451b77db6a18644a905ef284d7d09
sandarunihara <sandarunihara56@gmail.com>
2024-07-07 21:15:05 +0530

    1

--- lab4.scala
+++ lab4.scala
@@ -1,17 +1,26 @@
 
-  def display(itemNames: Array[String], itemQuantities: Array[Int]): Unit = {
+  def displayInventory(itemNames: Array[String], itemQuantities: Array[Int]): Unit = {
     if (itemNames.length != itemQuantities.length) {
       println("Error: The length of item names array and item quantities array must be the same.")
     } else {
       println("Inventory:")
-      //for (i <- itemNames.indices) { This is a method available on arrays (and other collections) that returns a range of all valid indices for the array
-        for (i <- 0 until itemNames.length){
+      for (i <- 0 until itemNames.length){
         println(s"${itemNames(i)}: ${itemQuantities(i)}")
       }
     }
   }
 
+  def restockItem(itemNames: Array[String], itemQuantities: Array[Int], itemName: String, quantity: Int): Unit = {
+    val index = itemNames.indexOf(itemName)
+    if (index >= 0) {
+      itemQuantities(index) += quantity
+      println(s"Restocked $quantity of $itemName.New quantity: ${itemQuantities(index)}")
+    } else {
+      println(s"Item '$itemName' does not exist in the inventory.")
+    }
+  }
+
   val items = Array("Apple", "Banana", "Orange")
-    val quantities = Array(10, 20, 15)
+  val quantities = Array(10, 20, 15)
 
  
