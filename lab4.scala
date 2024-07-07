89401ed96ab6ba7df63443f20d255eda0130992a
sandarunihara <sandarunihara56@gmail.com>
2024-07-07 21:01:40 +0530

    1

--- lab4.scala
+++ lab4.scala
@@ -1,19 +1,17 @@
-object InventoryManagement {
+
   def display(itemNames: Array[String], itemQuantities: Array[Int]): Unit = {
     if (itemNames.length != itemQuantities.length) {
       println("Error: The length of item names array and item quantities array must be the same.")
     } else {
       println("Inventory:")
-      for (i <- itemNames.indices) {
+      //for (i <- itemNames.indices) { This is a method available on arrays (and other collections) that returns a range of all valid indices for the array
+        for (i <- 0 until itemNames.length){
         println(s"${itemNames(i)}: ${itemQuantities(i)}")
       }
     }
   }
 
-  def main(args: Array[String]): Unit = {
-    val items = Array("Apple", "Banana", "Orange")
+  val items = Array("Apple", "Banana", "Orange")
     val quantities = Array(10, 20, 15)
 
-    display(items, quantities)
-  }
-}
+ 
