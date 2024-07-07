0d5fff979b6f595d292f041d828e2cee4ba7547f
sandarunihara <sandarunihara56@gmail.com>
2024-07-07 16:33:50 +0530

    1

--- lab4.scala
+++ lab4.scala
@@ -1,1 +1,19 @@
-var x=12
+object InventoryManagement {
+  def display(itemNames: Array[String], itemQuantities: Array[Int]): Unit = {
+    if (itemNames.length != itemQuantities.length) {
+      println("Error: The length of item names array and item quantities array must be the same.")
+    } else {
+      println("Inventory:")
+      for (i <- itemNames.indices) {
+        println(s"${itemNames(i)}: ${itemQuantities(i)}")
+      }
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val items = Array("Apple", "Banana", "Orange")
+    val quantities = Array(10, 20, 15)
+
+    display(items, quantities)
+  }
+}
